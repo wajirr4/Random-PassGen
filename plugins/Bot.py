74eefbe0b69b9994839e9e165aa32fa384fd3973
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-04 15:03:39 +0530

    Bot.py

---
+++ plugins/Bot.py
@@ -0,0 +1,27 @@
+import random, string
+from pyrogram import Client as sree
+from pyrogram import filters
+from pyrogram.types import Message
+from resources.str import (NUMARIC, alpha, ALPHA)
+
+
+@sree.on_message(filters.command('start', prefixes="/"))
+async def passgen(sree, m: Message):
+    await sree.send_message(m.chat.id, "i am jinda sur") 
+@sree.on_message(filters.command('pgen'))
+async def radniompass(sree, m: Message):
+    msgg = await m.reply('__Generating your password plish wait..__')
+    for i in range(3):
+        a = random.choice(ALPHA)
+    for i in range(3):
+        b = random.choice(alpha)
+    for i in range(2):
+        c = random.choice(NUMARIC)
+    for i in range(2):
+        symbol = string.punctuation
+        d = random.choice(symbol)
+    for i in range(10):
+        Pass = (f"{a}{b}{c}{d}")
+    P = ''.join(random.choice(Pass) for _ in range(10))
+    await msgg.edit_text(f"Your Password is {P}")
+  
