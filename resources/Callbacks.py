e37a89bb7d58e616b4eaf9d31f95586ed9541e0f
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-04 19:27:53 +0530

    Delete Callbacks.py

--- resources/Callbacks.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client as sree
-from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-
-@sree.on_callback_query()
-def close_(Client, cb: CallbackQuery):
-    if cb.data == "close_":
-        cb.answer("Menu Closed!", show_alert=True)
-        cb.m.delete()
