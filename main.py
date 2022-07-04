3fb88ff6a7acd239472a1f3e09c2900614752d58
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-04 15:02:45 +0530

    Main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+# created by ItsmeHyper13
+
+from pyrogram import Client
+from pyrogram import idle
+
+sree = Client(
+    ":memory:",
+    api_id=13014026,
+    api_hash="c1825666523834e125bcb8cf9f7b6318",
+    bot_token="5529184521:AAHfPAutxeKb3AZ_kxiMcvTmT7_NIMyPZWw",
+    plugins=dict(root="plugins")
+)
+
+print("Bot Logging Successfully✅")
+sree.start()
+idle()
