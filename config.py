7c6afcbf8f72e28b55bc6a0b03adf8f44b2e6559
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-04 15:10:21 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+# created by ItsmeHyper13
+
+
+from os import getenv
+from dotenv import load_dotenv
+
+API_ID = int(getenv("API_ID", "4110592"))
+API_HASH = getenv("API_HASH", "aa7c849566922168031b95212860ede0")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
