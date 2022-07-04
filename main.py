6e9a2c0ef672c93b398fc8182535577d9153c626
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-04 19:24:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ sree = Client(
 )
 
 print("Bot Logging Successfully✅")
-def main():
-    sree.send_message(-1001540434472, "Bot Started Successfully✅!")
+
+async def main(sree):
+    await sree.send_message(-1001540434472, "Bot Started Successfully✅!")
 
 
 sree.start()
