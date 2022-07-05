b135d7a01a72dc16935c828704802f915e44d895
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-05 14:46:53 +0530

    Update Callbacks.py

--- plugins/Callbacks.py
+++ plugins/Callbacks.py
@@ -1,9 +1,15 @@
+import random, string
 from pyrogram import Client as sree
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 @sree.on_callback_query()
-def close_(Client, cb: CallbackQuery):
+async def close_(Client, cb: CallbackQuery):
     if cb.data == "close_":
-        cb.answer("Menu Closed!")
-        cb.message.delete()
+        await cb.answer("Menu Closed!")
+        await cb.message.delete()
+    else cb.data == "ping_":
+        msgg = await cb.message.edit_text('__Generating another password plish wait..__')   
+        Pass = string.ascii_letters + string.digits + string.punctuation
+        P = ''.join(random.choice(Pass) for _ in range(10))
+        await cb.msgg.edit_text(f"<b><u>Your Password is Generated Successfully✅</u>\n\nPassword</b>:- <code>{P}</code>")
