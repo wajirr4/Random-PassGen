aa0e74c824c572891afa9fe949b490666e22531f
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-05 23:19:22 +0530

    Update Callbacks.py

--- plugins/Callbacks.py
+++ plugins/Callbacks.py
@@ -9,7 +9,17 @@ async def close_(Client, cb: CallbackQuery):
         await cb.answer("Menu Closed!")
         await cb.message.delete()
     elif cb.data == "chngpass1":
-        await cb.message.edit_text('__Generating another password plish wait..__')   
         Pass = string.ascii_letters + string.digits + string.punctuation
         P = ''.join(random.choice(Pass) for _ in range(10))
+        cback_data = cb.data.strip()
+        cback_req = cback_data.split(None, 1)[1]
+        query, user_id = cback_req.split("|")
+        if cb.from_user.id != int(user_id):
+            try:
+                return await cb.answer(
+                    "You are Not allowed to change this", show_alert=True
+                )
+            except:
+                return 
+        await cb.message.edit_text('__Generating another password plish wait..__')          
         await cb.message.edit_text(f"<b><u>Your Password is Generated Successfully✅</u>\n\nPassword</b>:- <code>{P}</code>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Change Password", callback_data="ping_")]]))
