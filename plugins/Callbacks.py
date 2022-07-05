1c3dafbf85b1a1259a794b48b576feb1864bb3d1
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-05 23:41:15 +0530

    Update Callbacks.py

--- plugins/Callbacks.py
+++ plugins/Callbacks.py
@@ -9,37 +9,9 @@ async def close_(Client, cb: CallbackQuery):
     if cb.data == "close_":
         await cb.answer("Menu Closed!")
         await cb.message.delete()
-'''
     elif cb.data == "chngpass1":
         Pass = string.ascii_letters + string.digits + string.punctuation
         P = ''.join(random.choice(Pass) for _ in range(10))
-        callback_data = CallbackQuery.data.strip()
-        callback_request = callback_data.split(None, 1)[1]
-        query, user_id = callback_request.split("|")
-        if cb.from_user.id != int(user_id):
-            try:
-                return await cb.answer(
-                    "You are Not allowed to change this", show_alert=True
-                )
-            except:
-                return 
-        await cb.message.edit_text('__Generating another password plish wait..__')          
+        await cb.message.edit_text('__Generating another password plish wait..__')
+        await cb.answer("Password Changed Successfully✅!")       
         await cb.message.edit_text(f"<b><u>Your Password is Generated Successfully✅</u>\n\nPassword</b>:- <code>{P}</code>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Change Password", callback_data="ping_")]]))
-'''
-
-@sree.on_callback_query(filters.regex("chngpass1"))
-async def command(_, CallbackQuery):
-    Pass = string.ascii_letters + string.digits + string.punctuation
-    P = ''.join(random.choice(Pass) for _ in range(10))
-    callback_data = CallbackQuery.data.strip()
-    callback_request = callback_data.split(None, 1)[1]
-    query, user_id = callback_request.split("|")
-    if CallbackQuery.from_user.id != int(user_id):
-        try:
-            return await CallbackQuery.answer(
-                "You're not allowed to close this.", show_alert=True
-            )
-        except:
-            return
-    await cb.message.edit_text('__Generating another password plish wait..__')          
-    await cb.message.edit_text(f"<b><u>Your Password is Generated Successfully✅</u>\n\nPassword</b>:- <code>{P}</code>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Change Password", callback_data="ping_")]]))
