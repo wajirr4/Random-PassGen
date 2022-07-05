b9ad941eccc7b80453423d5892b16153055487b3
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-05 14:52:40 +0530

    Update Callbacks.py

--- plugins/Callbacks.py
+++ plugins/Callbacks.py
@@ -8,7 +8,7 @@ async def close_(Client, cb: CallbackQuery):
     if cb.data == "close_":
         await cb.answer("Menu Closed!")
         await cb.message.delete()
-    else cb.data == "ping_":
+    elif cb.data == "ping_":
         msgg = await cb.message.edit_text('__Generating another password plish wait..__')   
         Pass = string.ascii_letters + string.digits + string.punctuation
         P = ''.join(random.choice(Pass) for _ in range(10))
