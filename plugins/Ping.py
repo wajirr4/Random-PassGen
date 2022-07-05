e06010f1c9d7431e16ed74a40c0780a654a553bf
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-05 14:03:16 +0530

    Update Ping.py

--- plugins/Ping.py
+++ plugins/Ping.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 from asyncio import sleep
 from pyrogram import Client as sree
 from pyrogram import filters
@@ -7,11 +8,13 @@ from pyrogram.types import Message
 
 @sree.on_message(filters.command(["ping", "p", "on"]))
 async def ping(sree, m: Message):
+    start = datetime.now()
     start_time = time.time()
     a = await m.reply("⚡")
     end_time = time.time()
     await sleep(1) 
     b = await a.edit_text("<b><i>Pinging...</i></b>")
     await sleep(1.5)
-    pong = str(round((end_time - start_time) * 1000, 3)) + " ms"
-    await b.edit_text("<b>Ping Pong! 🏓\n\nPing Time✅</b>: <code>{}</code>".format(pong))             
+    pong1 = (datetime.now() - start).microseconds / 1000
+    pong2 = str(round((end_time - start_time) * 1000, 3)) + " ms"
+    await b.edit_text("<b>Ping Pong! 🏓\n\nPing-1 Time✅: <code>{}</code>\nPing-2 Time✅</b>: <code>{}</code>".format(pong1, pong2))             
