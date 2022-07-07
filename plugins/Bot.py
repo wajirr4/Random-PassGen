b67da3164b1ee0da41aa9475fd2ab7ec768e4a6f
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-07 06:45:02 +0530

    huh

--- plugins/Bot.py
+++ plugins/Bot.py
@@ -1,14 +1,35 @@
 import random, string
 from pyrogram import Client as sree
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardButton
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from resources.str import (NUMARIC, alpha, ALPHA)
 
 
 @sree.on_message(filters.command('pgen'))
 async def radniompass(sree, m: Message):
     msgg = await m.reply('__Generating your password plish wait..__')
-    
+    btn1 = [
+        [
+            ('')
+            ('')
+        ],
+        [
+            ('')
+            ('')
+        ],
+        [
+            ('')
+            ('')
+        ],
+        [
+            ('')
+            ('')
+        ],
+        [
+            ('')
+            ('')
+        ],
+    ]
     Pass = string.ascii_letters + string.digits + string.punctuation
     P = ''.join(random.choice(Pass) for _ in range(10))
     await msgg.edit_text(f"<b><u>Your Password is Generated Successfully✅</u>\n\nPassword</b>:- <code>{P}</code>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Change Password", callback_data="chngpass1")]]))
