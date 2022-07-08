576c39f592a084ce421dc12c11f71554cd5b557d
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-08 09:48:37 +0530

    Update Bot.py

--- plugins/Bot.py
+++ plugins/Bot.py
@@ -1,3 +1,6 @@
+# Powered by @HYPER_AD13 | @ShiningOff
+# Dear Pero ppls Plish Don't remove this line from here🌚
+
 import random, string
 from pyrogram import Client as sree
 from pyrogram import filters
