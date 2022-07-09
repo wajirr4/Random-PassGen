b387a1a05d58b46baada0f27e234962a88da9255
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 04:32:38 +0530

    Update string.py

--- resources/string.py
+++ resources/string.py
@@ -1,1 +1,35 @@
+#from pyrogram import Client as sree
 
+pgentxt = """<b>Hey <code>{}</code> !</b>
+
+<i><u>Generate password as of your desires either
+strong medium or normal all are available, 
+read all password types an generate freely</u>.<i>
+
+<b>Here I distributed password into <code>10</code> category,  
+And with their <code>strength</code> !
+read and choose as you want to generate!!
+
+❅ Password-1
+    ➥ <code>{Normal}</code>
+❅ Password-2
+    ➥ <code>{Medium}</code>
+❅ Password-3
+    ➥ <code>{Very Strong}</code>
+❅ Password-4
+    ➥ <code>{Normal}</code>
+❅ Password-5
+    ➥ <code>{Normal}</code>
+❅ Password-6
+    ➥ <code>{Strong}</code>
+❅ Password-7
+    ➥ <code>{Strong}</code>
+❅ Password-8
+    ➥ <code>{Very Strong}</code>
+❅ Password-9
+    ➥ <code>{Very Strong}</code>
+❅ Password-10
+    ➥ <code>{Strong}</code>
+
+<i><u>Choose your types of password</u></i>!!   
+"""      
