c75a13b0b6fa58ac1a4362c0b267f148f388e4fd
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 05:07:34 +0530

    Create ReplyKeyboard.py

---
+++ resources/ReplyKeyboard.py
@@ -0,0 +1,26 @@
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+
+
+btn1 = [
+        [
+            ('Password-1'),
+            ('Password-2'),
+        ],
+        [
+            ('Password-3'),
+            ('Password-4'),
+        ],
+        [
+            ('Password-5'),
+            ('Password-6'),
+        ],
+        [
+            ('Password-7'),
+            ('Password-8'),
+        ],
+        [
+            ('Password-9'),
+            ('Password-10'),
+        ],
+    ]
+
