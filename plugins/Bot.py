4f76d5df8f8ffa1130ab638f587d19facfcc01e8
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 04:44:28 +0530

    Update Bot.py

--- plugins/Bot.py
+++ plugins/Bot.py
@@ -37,7 +37,7 @@ async def radniompass(sree, m: Message):
     #Pass = string.ascii_letters + string.digits + string.punctuation
     #P = ''.join(random.choice(Pass) for _ in range(10))
     #await msgg.edit_text(f"<b><u>Your Password is Generated Successfully✅</u>\n\nPassword</b>:- <code>{P}</code>", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Change Password", callback_data="chngpass1")]]))
-    await m.reply(pgentxt.format(nm), reply_markup=ReplyKeyboardMarkup(btn1, one_time_keyboard=True, resize_keyboard=True))
+    await m.reply(pgentxt.format(nm), reply_markup=ReplyKeyboardMarkup(btn1, one_time_keyboard=True))
 
 
 
