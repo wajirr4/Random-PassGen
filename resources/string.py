1954b751eda9231ee023357f111dd57132164801
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 05:12:00 +0530

    Update string.py

--- resources/string.py
+++ resources/string.py
@@ -11,25 +11,25 @@ And with their <code>strength</code> !
 read and choose as you want to generate!!
 
 ❅ Password-1
-    ➥ <code>{Strength : Normal}</code>
+    ➥ <code>[Strength : Normal]</code>
 ❅ Password-2
-    ➥ <code>{Strength : Medium}</code>
+    ➥ <code>[Strength : Medium]</code>
 ❅ Password-3
-    ➥ <code>{Strength : Very Strong}</code>
+    ➥ <code>[Strength : Very Strong]</code>
 ❅ Password-4
-    ➥ <code>{Strength : Normal}</code>
+    ➥ <code>[Strength : Normal]</code>
 ❅ Password-5
-    ➥ <code>{Strength : Normal}</code>
+    ➥ <code>[Strength : Normal]</code>
 ❅ Password-6
-    ➥ <code>{Strength : Strong}</code>
+    ➥ <code>[Strength : Strong]</code>
 ❅ Password-7
-    ➥ <code>{Strength : Strong}</code>
+    ➥ <code>[Strength : Strong]</code>
 ❅ Password-8
-    ➥ <code>{Strength : Very Strong}</code>
+    ➥ <code>[Strength : Very Strong]</code>
 ❅ Password-9
-    ➥ <code>{Strength : Very Strong}</code>
+    ➥ <code>[Strength : Very Strong]</code>
 ❅ Password-10
-    ➥ <code>{Strength : Strong}</code>
+    ➥ <code>[Strength : Strong]</code>
 
 <i><u>Choose your types of password</u></i>!!</b>
 
