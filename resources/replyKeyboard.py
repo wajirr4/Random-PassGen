0210decd16b775f27a28a8ea67a2ab45646a2f31
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 05:27:05 +0530

    Update replyKeyboard.py

--- resources/replyKeyboard.py
+++ resources/replyKeyboard.py
@@ -2,25 +2,177 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, 
 
 
 btn1 = [
-        [
-            ('Password-1'),
-            ('Password-2'),
-        ],
-        [
-            ('Password-3'),
-            ('Password-4'),
-        ],
-        [
-            ('Password-5'),
-            ('Password-6'),
-        ],
-        [
-            ('Password-7'),
-            ('Password-8'),
-        ],
-        [
-            ('Password-9'),
-            ('Password-10'),
-        ],
-    ]
+    [
+        ('Password-1'),
+        ('Password-2'),
+    ],
+    [
+        ('Password-3'),
+        ('Password-4'),
+    ],
+    [
+        ('Password-5'),
+        ('Password-6'),
+    ],
+    [
+        ('Password-7'),
+        ('Password-8'),
+    ],
+    [
+        ('Password-9'),
+        ('Password-10'),
+    ],
+]
+
+#--------------------------------------
+
+
+b1 = [
+    [
+        ('Password-1 (6)'),
+        ('Password-1 (8)'),
+    ],
+    [
+        ('Password-1 (10)'),
+        ('Password-1 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+b2 = [
+    [
+        ('Password-2 (6)'),
+        ('Password-2 (8)'),
+    ],
+    [
+        ('Password-2 (10)'),
+        ('Password-2 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b3 = [
+    [
+        ('Password-3 (6)'),
+        ('Password-3 (8)'),
+    ],
+    [
+        ('Password-3 (10)'),
+        ('Password-3 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b4 = [
+    [
+        ('Password-4 (6)'),
+        ('Password-4 (8)'),
+    ],
+    [
+        ('Password-4 (10)'),
+        ('Password-4 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b5 = [
+    [
+        ('Password-5 (6)'),
+        ('Password-5 (8)'),
+    ],
+    [
+        ('Password-5 (10)'),
+        ('Password-5 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+b6 = [
+    [
+        ('Password-6 (6)'),
+        ('Password-6 (8)'),
+    ],
+    [
+        ('Password-6 (10)'),
+        ('Password-6 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b7 = [
+    [
+        ('Password-7 (6)'),
+        ('Password-7 (8)'),
+    ],
+    [
+        ('Password-7 (10)'),
+        ('Password-7 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b8 = [
+    [
+        ('Password-8 (6)'),
+        ('Password-8 (8)'),
+    ],
+    [
+        ('Password-8 (10)'),
+        ('Password-8 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b9 = [
+    [
+        ('Password-9 (6)'),
+        ('Password-9 (8)'),
+    ],
+    [
+        ('Password-9 (10)'),
+        ('Password-9 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
+b10 = [
+    [
+        ('Password-10 (6)'),
+        ('Password-10 (8)'),
+    ],
+    [
+        ('Password-10 (10)'),
+        ('Password-10 (12)'),
+    ],
+    [
+        ('••Go To Home Menu••')
+    ],
+]
+
+
 
