5c811341af548dcc234ec2dfadb615dca1a37cf5
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 16:01:58 +0530

    Update mainstuff.py

--- plugins/mainstuff.py
+++ plugins/mainstuff.py
@@ -1,7 +1,7 @@
 from asyncio import sleep
 import platform
 from resources.string import helptxt, repotxt
-from resources.Buttons import strtbtn
+from plugins.Buttons import homebtn
 from pyrogram import Client as sree
 from pyrogram import filters, __version__ as pyro
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
@@ -13,7 +13,10 @@ async def help(sree, m: Message):
     uid = m.from_user.id
     chtid = m.chat.id
     await m.reply_sticker('CAACAgUAAx0CWOSA3AABBtl5YspfsV3UeUpFs3pfmeoy0UMM_tMAAn8FAAIvIkBW70JZlNo13zcpBA')
-    await m.reply(helptxt.format(nm), reply_markup=InlineKeyboardMarkup(homebtn))
+    await m.reply(
+        helptxt.format(nm),
+        reply_markup=InlineKeyboardMarkup(homebtn),
+    )
 
 @sree.on_message(filters.command(['repo', 'source']))
 async def sourcecd(sree, m: Message):
@@ -21,4 +24,7 @@ async def sourcecd(sree, m: Message):
     uid = m.from_user.id
     chtid = m.chat.id
     await m.reply_sticker('CAACAgUAAx0CWOSA3AABBtl5YspfsV3UeUpFs3pfmeoy0UMM_tMAAn8FAAIvIkBW70JZlNo13zcpBA')
-    await m.reply(repotxt.format(nm), reply_markup=InlineKeyboardMarkup(homebtn))
+    await m.reply(
+        repotxt.format(nm),
+        reply_markup=InlineKeyboardMarkup(homebtn)
+    )
