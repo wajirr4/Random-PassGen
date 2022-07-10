dbb3027efb1e342fef3a675e71aba294682f6123
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 16:25:35 +0530

    Update mainstuff.py

--- plugins/mainstuff.py
+++ plugins/mainstuff.py
@@ -1,8 +1,9 @@
 from asyncio import sleep
 import platform
-from resources.string import repotxt
+from resources.string import repotxt, helptxt
 from plugins.Buttons import homebtn
 from pyrogram import Client as sree
+from config import BOT_USERNAME
 from pyrogram import filters, __version__ as pyro
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
@@ -14,7 +15,7 @@ async def help(sree, m: Message):
     chtid = m.chat.id
     await m.reply_sticker('CAACAgUAAx0CWOSA3AABBtl5YspfsV3UeUpFs3pfmeoy0UMM_tMAAn8FAAIvIkBW70JZlNo13zcpBA')
     await m.reply(
-        f"""<b>Meow <code>{nm}</code> .</b>\n\n<b><u>Commands</u></b>\n/start : To Start the bot.\n/help : To get Help Menu.\n/ping or /on : To check Alive and ping of bot.\n/repo or /source : To get Source Code of @{} .\n/pgen : Use and explorer.\n    ➥ <code>Work in PM only</code>\n/sgen : Use and explorer.\n    ➥ <code>Work in Group only</code>""",
+        helptxt.format(nm, BOT_USERNAME),
         reply_markup=InlineKeyboardMarkup(homebtn),
         disable_web_page_preview=True,
     )
