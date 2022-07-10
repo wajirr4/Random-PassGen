7188e21fccb6419ae39b75773a014159cb3e91f1
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 05:59:27 +0530

    Update string.py

--- resources/string.py
+++ resources/string.py
@@ -1,6 +1,6 @@
 #from pyrogram import Client as sree
 
-pgentxt = """<b>Hey <code>{}</code> !</b>
+pgentxt = """<b>Meow Meow <code>{}</code> !</b>
 
 <i><u>Generate password as of your desires either
 strong medium or normal all are available, 
@@ -34,19 +34,172 @@ read and choose as you want to generate!!
 <i><u>Choose your types of password</u></i>!!</b>
 
 ➥ <i>Developed By @ShiningOff</i>  
-"""      
+"""  
+
+    
 
 #----------------------------------------–---------------
 
-txt1 = """
+
+
+t1 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-1</code> Category!</b>
+
+❅ Password-1
+    ➥ <code>[Strength : Normal]</code>
+
+<b>The strength of these type of password is <code>Normal</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
 """
 
 
+t2 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-2</code> Category!</b>
+
+❅ Password-2
+    ➥ <code>[Strength : Medium]</code>
+
+<b>The strength of these type of password is <code>Medium</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
 
 
+t3 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-3</code> Category!</b>
+
+❅ Password-3
+    ➥ <code>[Strength : Very Strong]</code>
+
+<b>The strength of these type of password is <code>Very Strong</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
 
 
+t4 = """<b>Woi Woi <code>{}</code> !!</b>
 
+<b>Meow You Choosen <code>Password-4</code> Category!</b>
+
+❅ Password-4
+    ➥ <code>[Strength : Normal]</code>
+
+<b>The strength of these type of password is <code>Normal</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
+
+
+t5 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-5</code> Category!</b>
+
+❅ Password-5
+    ➥ <code>[Strength : Normal]</code>
+
+<b>The strength of these type of password is <code>Normal</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
+
+
+t6 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-6</code> Category!</b>
+
+❅ Password-6
+    ➥ <code>[Strength : Strong]</code>
+
+<b>The strength of these type of password is <code>Strong</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
+
+
+t7 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-7</code> Category!</b>
+
+❅ Password-7
+    ➥ <code>[Strength : Strong]</code>
+
+<b>The strength of these type of password is <code>Strong</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
+
+
+t8 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-8</code> Category!</b>
+
+❅ Password-8
+    ➥ <code>[Strength : Very Strong]</code>
+
+<b>The strength of these type of password is <code>Very Strong</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
+
+
+t9 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-9</code> Category!</b>
+
+❅ Password-9
+    ➥ <code>[Strength : Very Strong]</code>
+
+<b>The strength of these type of password is <code>Very Strong</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
+
+
+t10 = """<b>Woi Woi <code>{}</code> !!</b>
+
+<b>Meow You Choosen <code>Password-10</code> Category!</b>
+
+❅ Password-10
+    ➥ <code>[Strength : Strong]</code>
+
+<b>The strength of these type of password is <code>Strong</code>!!</b>  
+
+<i><u>Choose The limits of password like do u wana password of <code>6</code>, <code>8</code>, <code>10</code>, <code>12</code> Digits And select
+Your option !!</u></i>!!</b>
+
+➥ <i>Developed By @ShiningOff</i>              
+"""
 
 
 
