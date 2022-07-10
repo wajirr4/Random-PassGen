a9ed22bb56c3fe1c86e327a8e2c4a2f7a408f16c
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 10:49:19 +0530

    Update Buttons.py

--- resources/Buttons.py
+++ resources/Buttons.py
@@ -4,19 +4,15 @@ from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 strtbtn = [
     [
         InlineKeyboardButton(
-            'Creator',
-            url='tg://user?id=5313879570'
-        ),
-        InlineKeyboardButton(
             'Support',
             url="https://silentverse"
         ),
-    ],
-    [
         InlineKeyboardButton(
             'Source',
             callback_data="repo_"
         ),
+    ],
+    [        
         InlineKeyboardButton(
             'Commands',
             callback_data="help_"
@@ -26,13 +22,9 @@ strtbtn = [
 
 
 homebtn = [
-    [
+    [      
         InlineKeyboardButton(
-            'Creator',
-            url='tg://user?id=5280801259'
-        ),
-        InlineKeyboardButton(
-            'Home!',
+            'Go Home!',
             callback_data="home_"
         ),
     ]
