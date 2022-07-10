a8d9b74e598fe6ec8f1f82cfc1463215680fcdca
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 06:52:57 +0530

    Update replyKeyboard.py

--- resources/replyKeyboard.py
+++ resources/replyKeyboard.py
@@ -27,14 +27,15 @@ btn1 = [
 #--------------------------------------
 
 
+
 b1 = [
     [
-        ('GenPass-1 (6)'),
-        ('Generates-1 (8)'),
+        ('GenPass-1-6'),
+        ('Generates-1-8'),
     ],
     [
-        ('Generate-1 (10)'),
-        ('GenPas-1 (12)'),
+        ('Generate-1-10'),
+        ('GenPas-1-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -43,12 +44,12 @@ b1 = [
 
 b2 = [
     [
-        ('GenPass-2 (6)'),
-        ('Generates-2 (8)'),
+        ('GenPass-2-6'),
+        ('Generates-2-8'),
     ],
     [
-        ('Generate-2 (10)'),
-        ('GenPas-2 (12)'),
+        ('Generate-2-10'),
+        ('GenPas-2-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -58,12 +59,12 @@ b2 = [
 
 b3 = [
     [
-        ('GenPass-3 (6)'),
-        ('Generates-3 (8)'),
+        ('GenPass-3-6'),
+        ('Generates-3-8'),
     ],
     [
-        ('Generate-3 (10)'),
-        ('GenPas-3 (12)'),
+        ('Generate-3-10'),
+        ('GenPas-3-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -73,12 +74,12 @@ b3 = [
 
 b4 = [
     [
-        ('GenPass-4 (6)'),
-        ('Generates-4 (8)'),
+        ('GenPass-4-6'),
+        ('Generates-4-8'),
     ],
     [
-        ('Generate-4 (10)'),
-        ('GenPas-4 (12)'),
+        ('Generate-4-10'),
+        ('GenPas-4-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -88,12 +89,12 @@ b4 = [
 
 b5 = [
     [
-        ('GenPass-5 (6)'),
-        ('Generates-5 (8)'),
+        ('GenPass-5-6'),
+        ('Generates-5-8'),
     ],
     [
-        ('Generate-5 (10)'),
-        ('GenPas-5 (12)'),
+        ('Generate-5-10'),
+        ('GenPas-5-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -102,12 +103,12 @@ b5 = [
 
 b6 = [
     [
-        ('GenPass-6 (6)'),
-        ('Generates-6 (8)'),
+        ('GenPass-6-6'),
+        ('Generates-6-8'),
     ],
     [
-        ('Generate-6 (10)'),
-        ('GenPas-6 (12)'),
+        ('Generate-6-10'),
+        ('GenPas-6-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -117,12 +118,12 @@ b6 = [
 
 b7 = [
     [
-        ('GenPass-7 (6)'),
-        ('Generates-7 (8)'),
+        ('GenPass-7-6'),
+        ('Generates-7-8'),
     ],
     [
-        ('Generate-7 (10)'),
-        ('GenPas-7 (12)'),
+        ('Generate-7-10'),
+        ('GenPas-7-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -132,12 +133,12 @@ b7 = [
 
 b8 = [
     [
-        ('GenPass-8 (6)'),
-        ('Generates-8 (8)'),
+        ('GenPass-8-6'),
+        ('Generates-8-8'),
     ],
     [
-        ('Generate-8 (10)'),
-        ('GenPas-8 (12)'),
+        ('Generate-8-10'),
+        ('GenPas-8-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -147,12 +148,12 @@ b8 = [
 
 b9 = [
     [
-        ('GenPass-9 (6)'),
-        ('Generates-9 (8)'),
+        ('GenPass-9-6'),
+        ('Generates-9-8'),
     ],
     [
-        ('Generate-9 (10)'),
-        ('GenPas-9 (12)'),
+        ('Generate-9-10'),
+        ('GenPas-9-12'),
     ],
     [
         ('••Go To Home Menu••')
@@ -162,17 +163,14 @@ b9 = [
 
 b10 = [
     [
-        ('GenPass-10 (6)'),
-        ('Generates-10 (8)'),
+        ('GenPass-10-6'),
+        ('Generates-10-8'),
     ],
     [
-        ('Generate-10 (10)'),
-        ('GenPas-10 (12)'),
+        ('Generate-10-10'),
+        ('GenPas-10-12'),
     ],
     [
         ('••Go To Home Menu••')
     ],
 ]
-
-
-
