66e535876cb460089dd9fb3718c355ee9887f578
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 10:31:22 +0530

    Update Buttons.py

--- resources/Buttons.py
+++ resources/Buttons.py
@@ -14,7 +14,7 @@ strtbtn = [
     ],
     [
         InlineKeyboardButton(
-            'Commands',
+            'Source',
             callback_data="repo_"
         ),
         InlineKeyboardButton(
@@ -22,4 +22,18 @@ strtbtn = [
             callback_data="help_"
         ),
     ],
+]
+
+
+homebtn = [
+    [
+        InlineKeyboardButton(
+            'Creator',
+            url='tg://user?id=5280801259'
+        ),
+        InlineKeyboardButton(
+            'Home!',
+            callback_data="home_"
+        ),
+    ]
 ]
