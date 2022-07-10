30fcd20381535ee1723018367597bbd1cc3ec1f9
!--ɪτs-ʜʏρєʀ-- <itsmehyper1706@gmail.com>
2022-07-10 10:34:26 +0530

    Update Start.py

--- plugins/Start.py
+++ plugins/Start.py
@@ -1,5 +1,6 @@
 from asyncio import sleep
 import platform
+from resources.Buttons import strtbtn
 from pyrogram import Client as sree
 from pyrogram import filters, __version__ as pyro
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
@@ -10,12 +11,12 @@ async def start(sree, m: Message):
     nm = m.from_user.first_name
     uid = m.from_user.id
     chtid = m.chat.id
-    a = await sree.send_message(chtid, f"Hey, [{nm}](tg://user?id={uid}) 🙋")
+    await m.reply_sticker("CAACAgUAAx0CWOSA3AABBtloYspMfkBVg1TaL_du9BzO-Qc_E8QAAqsGAAJmnjhW4cdNUQVx0CUpBA")
+    a = await m.reply(chtid, f"Hey, [{nm}](tg://user?id={uid}) 🙋")
     await sleep(0.5)
     b = await a.edit_text("Starting Bot Server For You..")
     await sleep(1)
     c = await b.edit_text("Server Started Successfully ✅\n\nHere we Go🥳!!")
     await sleep(0.4)
-    await c.edit_text(statxt.format(nm, platform.python_version(), pyro))
-    await sleep(0.2)
-    await m.delete()   
+    await c.edit_text(statxt.format(nm, platform.python_version(), pyro), reply_markup=InlineKeyboardMarkup(strtbtn))     
+    
